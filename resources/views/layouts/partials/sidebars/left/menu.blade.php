52960846ce719158f0f698a58a3cee86f23b7b18
dimitri <dimitri.dacosta34@gmail.com>
2018-05-31 20:21:01 +0200

    Sections added to sidebar menu

--- resources/views/layouts/partials/sidebars/left/menu.blade.php
+++ resources/views/layouts/partials/sidebars/left/menu.blade.php
@@ -1,31 +1,41 @@
 <div class="menu">
     <ul class="list">
-        @section('sidebar-menu')
-        <ul class="list">
-            <li class="header">{{ strtoupper(uctrans('menu.title', $module)) }}</li>            
-        </ul>
-        @if (isset($domain))
-        {{-- Home module --}}
-        <li @if ('home' === $module->name)class="active"@endif>
-            <a href="/{{ $domain->slug }}/home">
-                <i class="material-icons">home</i>
-                <span>{{ uctrans('uccello::home.home') }}</span>
-            </a>
-        </li>
-        @endif
-        {{-- All modules except Home --}}
-        @if (isset($modules) && isset($domain) && isset($module))
-            @foreach ($modules as $_module)
-                @if ($_module->name !== 'home')
-                <li @if ($_module->id === $module->id)class="active"@endif>
-                    <a href="{{ route('list', ['domain' => $domain->slug, 'module' => $_module->name]) }}">
-                        @if ($_module->icon)<i class="material-icons">{{ $_module->icon ?? 'list' }}</i>@endif
-                        <span>{{ uctrans($_module->name, $_module) }}</span>
-                    </a>
-                </li>
-                @endif
-            @endforeach
-        @endif
+        @yield('sidebar-menu-before')
+
+        @section('sidebar-main-menu')
+            <ul class="list">
+                <li class="header">{{ strtoupper(uctrans('menu.title', $module)) }}</li>            
+            </ul>
+            
+            @yield('sidebar-main-menu-before')
+
+            @if (isset($domain))
+            {{-- Home module --}}
+            <li @if ('home' === $module->name)class="active"@endif>
+                <a href="/{{ $domain->slug }}/home">
+                    <i class="material-icons">home</i>
+                    <span>{{ uctrans('uccello::home.home') }}</span>
+                </a>
+            </li>
+            @endif
+            
+            {{-- All modules except Home --}}
+            @if (isset($modules) && isset($domain) && isset($module))
+                @foreach ($modules as $_module)
+                    @if ($_module->name !== 'home')
+                    <li @if ($_module->id === $module->id)class="active"@endif>
+                        <a href="{{ route('list', ['domain' => $domain->slug, 'module' => $_module->name]) }}">
+                            @if ($_module->icon)<i class="material-icons">{{ $_module->icon ?? 'list' }}</i>@endif
+                            <span>{{ uctrans($_module->name, $_module) }}</span>
+                        </a>
+                    </li>
+                    @endif
+                @endforeach
+            @endif
+
+            @yield('sidebar-main-menu-after')
         @show
+
+        @yield('sidebar-menu-after')
     </ul>
 </div>

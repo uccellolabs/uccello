ccbfc9eb9dfbab39ca6b33201fae26af65b21f6a
Jonathan SARDO <sardoj@gmail.com>
2018-11-08 12:03:28 -0400

    fix (list): Improves the closing of dropdown

--- resources/views/modules/default/list/main.blade.php
+++ resources/views/modules/default/list/main.blade.php
@@ -49,7 +49,7 @@
     @show
 
     <div class="row clearfix">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="min-height: 600px">
             <div class="card">
                 <div class="body">
                     <div class="table-responsive">

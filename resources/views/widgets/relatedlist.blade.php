c410a123f424dbc7292d91086f09f4766ee4fa98
Jonathan <sardoj@gmail.com>
2019-02-06 12:32:17 +0100

    fix(widget): Improves widgets display (#23)

--- resources/views/widgets/relatedlist.blade.php
+++ resources/views/widgets/relatedlist.blade.php
@@ -7,10 +7,12 @@
         $relatedlist = \Uccello\Core\Models\Relatedlist::find($data->id);
     ?>
     @if(!is_null($relatedlist) && Auth::user()->canRetrieve($domain, $relatedlist->relatedModule))
-    <div class="col-sm-12">
-        <div class="dataTable-container m-b-20" id="relatedlist_{{ $relatedlist->relatedModule->name }}_{{ $relatedlist->id }}_widget" data-button-size="mini">
-            {{-- Card --}}
-            @include('uccello::modules.default.detail.relatedlists.card')
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="dataTable-container m-b-20" id="relatedlist_{{ $relatedlist->relatedModule->name }}_{{ $relatedlist->id }}_widget" data-button-size="mini">
+                {{-- Card --}}
+                @include('uccello::modules.default.detail.relatedlists.card')
+            </div>
         </div>
     </div>
     @endif

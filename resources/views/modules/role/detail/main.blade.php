0dd7896666a0d0a0f3d0b3788f316589e3bb8d7d
Jonathan SARDO <sardoj@gmail.com>
2019-04-23 20:56:38 +0200

    fix(settings): Improves ui

--- resources/views/modules/role/detail/main.blade.php
+++ resources/views/modules/role/detail/main.blade.php
@@ -15,9 +15,9 @@
         <div class="row">
             <div class="col s12">
                 @forelse ($record->profiles->where('domain_id', $domain->id) as $profile)
-                <a href="{{ ucroute('uccello.detail', $domain, 'profile', [ 'id' => $profile->id ]) }}" class="btn-small waves-effect primary">{{ $profile->name }}</a>
+                <a href="{{ ucroute('uccello.detail', $domain, 'profile', [ 'id' => $profile->id ]) }}" class="btn-flat waves-effect primary-text">{{ $profile->name }}</a>
                 @empty
-                <span class="red white-text" style="padding: 5px">{{ uctrans('label.no_profile', $module) }}</span>
+                <span class="red-text" style="padding: 5px">{{ uctrans('label.no_profile', $module) }}</span>
                 @endforelse
             </div>
         </div>
--- resources/views/modules/user/detail/main.blade.php
+++ resources/views/modules/user/detail/main.blade.php
@@ -15,9 +15,9 @@
         <div class="row">
             <div class="input-field col s12">
                 @forelse ($record->rolesOnDomain($domain) as $role)
-                <a href="{{ ucroute('uccello.detail', $domain, 'role', [ 'id' => $role->id ]) }}" class="btn-small waves-effect primary">{{ $role->name }}</a>
+                <a href="{{ ucroute('uccello.detail', $domain, 'role', [ 'id' => $role->id ]) }}" class="btn-flat waves-effect primary-text">{{ $role->name }}</a>
                 @empty
-                <span class="red white-text" style="padding: 5px">{{ uctrans('label.no_role', $module) }}</span>
+                <span class="red-text" style="padding: 5px">{{ uctrans('label.no_role', $module) }}</span>
                 @endforelse
             </div>
         </div>

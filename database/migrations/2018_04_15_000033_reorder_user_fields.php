a50a76b3875da14d1ecd382ee476e722fa6854d2
Jonathan SARDO <sardoj@gmail.com>
2019-09-02 14:17:59 +0200

    fix(user): Reorders fields

---
+++ database/migrations/2018_04_15_000033_reorder_user_fields.php
@@ -0,0 +1,78 @@
+<?php
+
+use Uccello\Core\Database\Migrations\Migration;
+use Uccello\Core\Models\Module;
+use Uccello\Core\Models\Field;
+
+class ReorderUserFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $userModule = Module::where('name', 'user')->first();
+
+        $emailField = Field::where('module_id', $userModule->id)
+            ->where('name', 'email')
+            ->first();
+        $emailField->sequence = 3;
+        $emailField->save();
+
+        $passwordField = Field::where('module_id', $userModule->id)
+            ->where('name', 'password')
+            ->first();
+        $passwordField->sequence = 4;
+        $passwordField->save();
+
+        $nameField = Field::where('module_id', $userModule->id)
+            ->where('name', 'name')
+            ->first();
+        $nameField->data = [ 'icon' => 'person' ];
+        $nameField->save();
+
+        $usernameField = Field::where('module_id', $userModule->id)
+            ->where('name', 'username')
+            ->first();
+        $usernameField->data = [ 'rules' => 'required|regex:/^[a-zA-Z0-9.-_]+$/|unique:users,username,%id%', 'icon' => 'vpn_key' ];
+        $usernameField->save();
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $userModule = Module::where('name', 'user')->first();
+
+        $emailField = Field::where('module_id', $userModule->id)
+            ->where('name', 'email')
+            ->first();
+        $emailField->sequence = 4;
+        $emailField->save();
+
+
+        $passwordField = Field::where('module_id', $userModule->id)
+            ->where('name', 'password')
+            ->first();
+        $passwordField->sequence = 3;
+        $passwordField->save();
+
+        $nameField = Field::where('module_id', $userModule->id)
+            ->where('name', 'name')
+            ->first();
+        $nameField->data = null;
+        $nameField->save();
+
+        $usernameField = Field::where('module_id', $userModule->id)
+            ->where('name', 'username')
+            ->first();
+        $usernameField->data = [ 'rules' => 'required|regex:/^[a-zA-Z0-9.-_]+$/|unique:users,username,%id%', 'icon' => 'person' ];
+        $usernameField->save();
+    }
+}

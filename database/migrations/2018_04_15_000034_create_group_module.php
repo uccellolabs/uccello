05921f935818bf07c7b30a5213e1dbacfa85c054
Dimitri DA COSTA <dimitri.dacosta34@gmail.com>
2019-10-02 16:35:38 +0200

    BugFix: Group's name field

--- database/migrations/2018_04_15_000034_create_group_module.php
+++ database/migrations/2018_04_15_000034_create_group_module.php
@@ -142,7 +142,7 @@ class CreateGroupModule extends Migration
         $field->name = 'name';
         $field->uitype_id = uitype('text')->id;
         $field->displaytype_id = displaytype('everywhere')->id;
-        $field->data = [ 'rules' => 'required|regex:/(?!^\d+$)^.+$/|unique:'.$this->getTablePrefix().'domains,name,%id%' ];
+        $field->data = null;
         $field->sequence = 0;
         $field->block_id = $block->id;
         $field->module_id = $module->id;

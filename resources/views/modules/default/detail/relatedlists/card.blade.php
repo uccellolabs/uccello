98e3580da0a11218227c54dcd6c9309a6379db61
Jonathan SARDO <sardoj@gmail.com>
2019-11-11 12:07:09 -0300

    feat(relatedlist): Adds search and loading #36

--- resources/views/modules/default/detail/relatedlists/card.blade.php
+++ resources/views/modules/default/detail/relatedlists/card.blade.php
@@ -1,10 +1,15 @@
 
 <?php
-    $datatableId = $datatableId.'-'.$relatedlist->id;
-    $datatableColumns = Uccello::getDatatableColumns($relatedlist->relatedModule, null, 'related-list');
-    $datatableFilter = Uccello::getDefaultFilter($relatedlist->relatedModule, 'related-list');
+$datatableId = $datatableId.'-'.$relatedlist->id;
+$datatableColumns = Uccello::getDatatableColumns($relatedlist->relatedModule, null, 'related-list');
+$datatableFilter = Uccello::getDefaultFilter($relatedlist->relatedModule, 'related-list');
 ?>
 <div class="card relatedlist">
+    {{-- Loader top --}}
+    <div class="progress transparent loader" data-table="{{ $datatableId }}" style="margin: 0">
+        <div class="indeterminate green"></div>
+    </div>
+
     <div class="card-content">
         {{-- Title --}}
         <span class="card-title">
@@ -21,5 +26,26 @@
 
         {{-- Table --}}
         @include('uccello::modules.default.detail.relatedlists.table', [ 'datatableId' => $datatableId, 'datatableColumns' => $datatableColumns, 'relatedModule' => $relatedlist->relatedModule, 'selectedFilter' => $datatableFilter ])
+
+        {{-- Loader bottom --}}
+        <div class="loader center-align" data-table="{{ $datatableId }}">
+            <div class="preloader-wrapper big active">
+                <div class="spinner-layer spinner-primary-only">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                {{ uctrans('datatable.loading', $module) }}
+            </div>
+        </div>
     </div>
 </div>
--- resources/views/modules/default/detail/relatedlists/selection-modal/content.blade.php
+++ resources/views/modules/default/detail/relatedlists/selection-modal/content.blade.php
@@ -4,10 +4,36 @@
 ?>
 @if ($relatedlist->canSelect() && Auth::user()->canRetrieve($domain, $relatedlist->relatedModule))
     <div class="selection-modal-content hide" data-relatedlist="{{ $relatedlist->id }}">
+        {{-- Loader top --}}
+        <div class="progress transparent loader" data-table="{{ $datatableId }}" style="margin: 0">
+            <div class="indeterminate green"></div>
+        </div>
+
         <div class="row">
             <div class="col s12">
                 {{-- Table --}}
-                @include('uccello::modules.default.detail.relatedlists.table', [ 'datatableId' => $datatableId, 'datatableContentUrl' => ucroute('uccello.list.content', $domain, $relatedlist->relatedModule, ['action' => 'select']), 'relatedModule' => $relatedlist->relatedModule ])
+                @include('uccello::modules.default.detail.relatedlists.table', [ 'datatableId' => $datatableId, 'datatableContentUrl' => ucroute('uccello.list.content', $domain, $relatedlist->relatedModule, ['action' => 'select']), 'relatedModule' => $relatedlist->relatedModule, 'searchable' => true ])
+            </div>
+        </div>
+
+        {{-- Loader bottom --}}
+        <div class="loader center-align" data-table="{{ $datatableId }}">
+            <div class="preloader-wrapper big active">
+                <div class="spinner-layer spinner-primary-only">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                {{ uctrans('datatable.loading', $module) }}
             </div>
         </div>
     </div>

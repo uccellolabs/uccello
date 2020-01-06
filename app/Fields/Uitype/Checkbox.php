33cec495b5fdc9c647b1963a0e194070f04653ea
Jonathan SARDO <sardoj@gmail.com>
2020-01-06 15:25:00 +0100

    fix(uitype): Sets default value for checkbox

--- app/Fields/Uitype/Checkbox.php
+++ app/Fields/Uitype/Checkbox.php
@@ -61,7 +61,7 @@ class Checkbox implements Uitype
      */
     public function getFormattedValueToSave(Request $request, Field $field, $value, $record = null, ?Domain $domain = null, ?Module $module = null) : ?string
     {
-        return isset($value) ? '1' : null;
+        return isset($value) ? '1' : '0';
     }
 
     /**
